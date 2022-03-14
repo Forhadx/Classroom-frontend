0464b79d52610571cdbc50d208c60d271d14f8d7
forhadx <forhadsh1@gmail.com>
2022-03-15 00:37:02 +0600

    fix logo

--- components/Layout/Navigation.js
+++ components/Layout/Navigation.js
@@ -11,7 +11,7 @@ export default function NavigationPage() {
           <li>
             <Link href="/faculty/rooms">
               <a>
-                <BsClipboardData />
+                <BsClipboardData /> Classroom
               </a>
             </Link>
           </li>
