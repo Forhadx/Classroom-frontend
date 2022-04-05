77297a75b8a3f2ba27562ef54844cf0eb3028fd2
forhadx <forhadsh1@gmail.com>
2022-04-06 05:23:45 +0600

    change cofig file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "classroom-forhadx.herokuapp.com"],
+    domains: ["classroom-forhadx.herokuapp.com"],
   },
 };
 
